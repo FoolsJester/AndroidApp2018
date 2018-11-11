6fc5a7c4a5dcbde9b6bb649bb878aaa6e8dafa34
Eimear Galligan <eimearni.gh@gmail.com>
2018-11-11 11:32:36 +0000

    Added CourseCode as first field in study time database

--- app/src/main/java/com/example/hp/androidproject/DatabaseHelper2.java
+++ app/src/main/java/com/example/hp/androidproject/DatabaseHelper2.java
@@ -12,9 +12,10 @@ public class DatabaseHelper2 extends SQLiteOpenHelper {
     public static final String DATABASE_NAME="StudyBudy.db";
     public static final String TABLE_NAME="StudyTimeLog";
     public static final String COL_1="Count_ID ";
-    public static final String COL_2="DateTime ";
-    public static final String COL_3="TotalStudy ";
-    public static final String COL_4="Interrupted ";
+    public static final String COL_2="CourseCode ";
+    public static final String COL_3="DateTime ";
+    public static final String COL_4="TotalStudy ";
+    public static final String COL_5="Interrupted ";
 
     public DatabaseHelper2(Context context) {
         super(context, DATABASE_NAME, null, 1);
@@ -22,7 +23,7 @@ public class DatabaseHelper2 extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE " + TABLE_NAME + "(" + COL_1 +"INTEGER PRIMARY KEY AUTOINCREMENT, "+ COL_2+ "VARCHAR, "+ COL_3+ " INTEGER, "+COL_4+ "INTEGER)");
+        db.execSQL("CREATE TABLE " + TABLE_NAME + "(" + COL_1 +"INTEGER PRIMARY KEY AUTOINCREMENT, "+ COL_2+ "VARCHAR, "+ COL_3+ "VARCHAR, "+COL_4+ "INTEGER, "+COL_5+ "INTEGER)");
     }
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
--- app/src/main/java/com/example/hp/androidproject/StudyTimer.java
+++ app/src/main/java/com/example/hp/androidproject/StudyTimer.java
@@ -7,13 +7,16 @@ import android.os.Bundle;
 import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Chronometer;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 
 public class StudyTimer extends AppCompatActivity {
 
@@ -31,11 +34,19 @@ public class StudyTimer extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.chronometer);
 
+        //Declaring variables
         openHelper = new DatabaseHelper2(this);
         simpleChronometer = (Chronometer) findViewById(R.id.simpleChronometer);
         startBtn = (Button) findViewById(R.id.btStart);
         pauseBtn = (Button) findViewById(R.id.btPause);
         finishBtn = (Button) findViewById(R.id.btFinish);
+        final Spinner dropdown = findViewById(R.id.spinner1);
+
+        //Initialising dropdown to display courses - hardcoded for now
+        String[] labels = {"COMP30650", "COMP50650", "COMP20650", "COMP40650", "COMP10650", "COMP60650"};
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, labels);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        dropdown.setAdapter(dataAdapter);
 
         //Start timer
         startBtn.setOnClickListener(new View.OnClickListener() {
@@ -69,10 +80,10 @@ public class StudyTimer extends AppCompatActivity {
             public void onClick(View v) {
                 saveTime = SystemClock.elapsedRealtime() - simpleChronometer.getBase();
                 int paused = Math.round(SystemClock.elapsedRealtime() - saveTime);
-                Toast.makeText(StudyTimer.this, ""+paused+"", Toast.LENGTH_SHORT).show();
+                String course = dropdown.getSelectedItem().toString();
+                Toast.makeText(StudyTimer.this, ""+paused+""+course, Toast.LENGTH_SHORT).show();
                 db=openHelper.getWritableDatabase();
-                insertData(studyStartTime, Math.round(saveTime/1000), paused);
-
+                insertData(course, studyStartTime, Math.round(saveTime/1000), paused);
                 simpleChronometer.setBase(SystemClock.elapsedRealtime());
                 stopTime = 0;
                 simpleChronometer.stop();
@@ -83,11 +94,13 @@ public class StudyTimer extends AppCompatActivity {
         });
     }
 
-    private void insertData(String SDate,int STime,int SInterrupt) {
+    //Insert data to database
+    private void insertData(String CourseCode, String SDate,int STime,int SInterrupt) {
         ContentValues contentValues = new ContentValues();
-        contentValues.put(DatabaseHelper2.COL_2, SDate);
-        contentValues.put(DatabaseHelper2.COL_3, STime);
-        contentValues.put(DatabaseHelper2.COL_4, SInterrupt);
+        contentValues.put(DatabaseHelper2.COL_2, CourseCode);
+        contentValues.put(DatabaseHelper2.COL_3, SDate);
+        contentValues.put(DatabaseHelper2.COL_4, STime);
+        contentValues.put(DatabaseHelper2.COL_5, SInterrupt);
         long count = db.insert(DatabaseHelper2.TABLE_NAME, null, contentValues);
         Toast.makeText(StudyTimer.this, STime+ " : study logged", Toast.LENGTH_SHORT).show();
     }
